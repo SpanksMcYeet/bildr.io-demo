91449cd47f5f5b0a11f23c83db65c4bc9649a916
Damocles <lunahackley@gmail.com>
2023-02-15 18:48:59 -0600

    Update events.js

--- public/events.js
+++ public/events.js
@@ -9,21 +9,28 @@ export let mouse = {
   left: false,
   right: false,
 }
+export let zoom = 0
 
 export let keys = []
 
-canvas.addEventListener('click', e => {
+canvas.addEventListener('mousedown', e => {
   mouse.left = true
+})
+canvas.addEventListener('mouseup', e => {
+  mouse.left = false
 })
 canvas.addEventListener('contextmenu', e => {
   e.preventDefault()
-  mouse.left = true
+  mouse.right = true
 })
 canvas.addEventListener('mousemove', e => {
   mouse.x = e.clientX * window.devicePixelRatio
   mouse.y = e.clientY * window.devicePixelRatio
 })
-
+canvas.addEventListener('wheel', e => {
+  e.preventDefault()
+  zoom -= Math.sign(e.deltaY)
+})
 canvas.addEventListener('keydown', e => {
   /*let key = null
   if (e.key === ) {
